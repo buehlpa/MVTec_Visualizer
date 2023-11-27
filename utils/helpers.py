60dc0412c5f6e4b71adca6bc4bbea3bd0fe3a7a2
Pascal <buehler1991@gmail.com>
2023-11-27 12:45:34 +0100

    fixed categoreis bug

--- utils/helpers.py
+++ utils/helpers.py
@@ -32,20 +32,19 @@ def create_synset_for_class(category:str,df,sampler=sample_synthetic_norm):
     anomaly_categories = {
         'bottle': ['broken_large', 'broken_small', 'contamination'],
         'cable': ['bent_wire', 'cable_swap', 'combined', 'cut_inner_insulation', 'cut_outer_insulation', 'missing_cable', 'missing_wire', 'poke_insulation'],
-        'capsule': ['crack', 'faulty_imprint', 'poke', 'squeeze'],
+        'capsule': ['crack', 'faulty_imprint', 'poke', 'scratch','squeeze'],
         'carpet': ['color', 'cut', 'hole', 'metal_contamination', 'thread'],
-        'grid': ['bent', 'broken', 'glue', 'metal_contamination'],
+        'grid': ['bent', 'broken', 'glue', 'metal_contamination', 'thread'],
         'hazelnut': ['crack', 'cut', 'hole', 'print'],
         'leather': ['color', 'cut', 'fold', 'glue', 'poke'],
         'metal_nut': ['bent', 'color', 'flip', 'scratch'],
-        'pill': ['color', 'contamination', 'crack', 'faulty_imprint', 'pill_type'],
-        'screw': ['manipulated_front', 'scratch_head', 'thread_side', 'thread_top'],
-        'tile': ['crack', 'glue_strip', 'gray_stroke', 'oil'],
+        'pill': ['color', 'combined','contamination', 'crack', 'faulty_imprint', 'pill_type','scratch'],
+        'screw': ['manipulated_front', 'scratch_head', 'scratch_neck','thread_side', 'thread_top'],
+        'tile': ['crack', 'glue_strip', 'gray_stroke', 'oil','rough'],
         'toothbrush': ['defective'],
-        'transistor': ['bent', 'cut', 'damaged_case', 'misplaced'],
+        'transistor': ['bent_lead', 'cut_lead', 'damaged_case', 'misplaced'],
         'wood': ['color', 'combined', 'hole', 'liquid', 'scratch'],
-        'zipper': ['broken_teeth', 'fabric_border', 'split_teeth', 'squeezed_teeth']
-    }
+        'zipper': ['broken_teeth', 'combined','fabric_border', 'fabric_interior','split_teeth','rough', 'squeezed_teeth']}
 
 
 
